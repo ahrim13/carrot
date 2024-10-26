07c80fc822fb90591c945381466b9f3bd9cecf04
ahrim13 <wsx014789@naver.com>
2024-10-27 01:58:43 +0900

    당근마켓 클론코딩
    - 글쓰기 업로드 구현

--- frontend/time.js
+++ frontend/time.js
@@ -6,12 +6,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const hours = String(now.getHours()).padStart(2, "0");
     const minutes = String(now.getMinutes()).padStart(2, "0");
 
-    // 현재 시간을 시:분 형식으로
     currentTimeElement.textContent = `${hours}:${minutes}`;
   }
 
   updateTime();
 
-  // 1분마다 시간 업데이트
-  setInterval(updateTime, 60000); // 60000 밀리초 = 1분
+  setInterval(updateTime, 60000);
 });
